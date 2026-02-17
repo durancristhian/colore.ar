9d9f2620c459d5ec8b779001987975030146b5d5
Cristhian Duran <durancristhian@gmail.com>
2026-02-17 12:55:06 -0300

    light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className="dark">
+      <html lang="en" /* className="dark" */>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased print:hidden`}
         >
