b6cbf8538461854ac2d456fd202bf94009bdb9a8
Cristhian Duran <durancristhian@gmail.com>
2026-02-23 11:48:26 -0300

    refactor blobToPng

--- components/image-actions-menu.tsx
+++ components/image-actions-menu.tsx
@@ -22,36 +22,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { usePrintImage } from "@/components/print-image-provider";
+import { blobToPng } from "@/utils/blob-to-png";
 import { buildImageDownloadFilename } from "@/utils/image-download-filename";
 import { downloadImage } from "@/utils/download-image";
-
-function blobToPng(blob: Blob): Promise<Blob> {
-  return new Promise((resolve, reject) => {
-    const url = URL.createObjectURL(blob);
-    const img = new Image();
-    img.onload = () => {
-      URL.revokeObjectURL(url);
-      const canvas = document.createElement("canvas");
-      canvas.width = img.naturalWidth;
-      canvas.height = img.naturalHeight;
-      const ctx = canvas.getContext("2d");
-      if (!ctx) {
-        reject(new Error("No 2d context"));
-        return;
-      }
-      ctx.drawImage(img, 0, 0);
-      canvas.toBlob(
-        (b) => (b ? resolve(b) : reject(new Error("toBlob failed"))),
-        "image/png",
-      );
-    };
-    img.onerror = () => {
-      URL.revokeObjectURL(url);
-      reject(new Error("Image load failed"));
-    };
-    img.src = url;
-  });
-}
 
 interface ImageActionsMenuProps {
   imageId: string;
