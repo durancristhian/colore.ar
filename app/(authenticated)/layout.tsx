5dbe75840349d3a9692ec59a733226d44b67d429
Cristhian Duran <durancristhian@gmail.com>
2026-02-23 17:14:34 -0300

    sparkles in the title

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { HeaderUserMenu } from "@/components/header-user-menu";
 import { PrintImageProvider } from "@/components/print-image-provider";
+import { SparklesText } from "@/components/ui/sparkles-text";
 
 export default function AuthenticatedLayout({
   children,
@@ -12,7 +13,9 @@ export default function AuthenticatedLayout({
       <div className="flex flex-col gap-4">
         <header className="flex items-center justify-between border-b pb-4">
           <Link href="/imagenes">
-            <h1 className="text-xl font-black">Colore.ar</h1>
+            <SparklesText as={<h1 />} className="text-xl font-black">
+              Colore.ar
+            </SparklesText>
           </Link>
           <HeaderUserMenu />
         </header>
